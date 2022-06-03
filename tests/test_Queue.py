0bca3c97dbe8165f20bab61580ae2748e6bf816d
bogavtk <67970256+bogavtk@users.noreply.github.com>
2022-06-03 18:08:58 +0300

    Update test_Queue.py

--- tests/test_Queue.py
+++ tests/test_Queue.py
@@ -9,15 +9,15 @@ queue = Queue()
 
 class Test_Queue:
     @pytest.fixture()
-    def create_human(self):
+    def person(self):
         name = "Marat"
         age = random.randint(15, 45)
         human = Human(name, age)
         queue.add_item(human)
         return human
 
-    def test_contains(self, create_human):
-        assert queue.contains(create_human) == create_human
+    def test_contains(self, person):
+        assert queue.contains(person) == person
 
-    def test_remove(self, create_human):
+    def test_remove(self):
         assert queue.remove() == queue.lenght
